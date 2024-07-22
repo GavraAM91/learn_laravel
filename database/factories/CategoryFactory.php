493eecb239c9979c3680be6358bc073788454bdd
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-07-22 15:20:00 +0700

    error solved ternyata hanya ganti tanggal saja

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,8 +18,8 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->sentence(rand(1, 2)),
-            'slug' => Str::slug(fake()->sentence(rand(1, 2))),
+            'name' => fake()->sentence(rand(1, 2), false),
+            'slug' => Str::slug(fake()->sentence(rand(1, 2), false)),
         ];
     }
 }
