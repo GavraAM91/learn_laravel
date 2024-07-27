6634465895fab0a38c626878134a12407018cc07
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-07-27 19:52:25 +0700

    adding spatie to my project

--- resources/views/crud/crud.blade.php
+++ resources/views/crud/crud.blade.php
@@ -20,23 +20,23 @@
                 <div class="grid gap-4 mb-4 sm:grid-cols-2 sm:gap-6 sm:mb-5">
                     <div class="sm:col-span-2">
                         <label for="title"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                            class="block mb-2 text-sm font-medium text-gray-900">Title</label>
                         <input type="text" name="title" id="title"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                             placeholder="title" required="">
                     </div>
                     <div class="w-full">
                         <label for="author"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">author</label>
+                            class="block mb-2 text-sm font-medium text-gray-900">author</label>
                         <input type="text" name="author" id="author"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                             placeholder="Author" required="">
                     </div>
                     <div>
                         <label for="category"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                            class="block mb-2 text-sm font-medium text-gray-900">Category</label>
                         <select id="category" name="category"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5">
                             <option value="1">Web design</option>
                             <option value="2">UI/UX</option>
                             <option value="3">Machine Learning</option>
@@ -46,16 +46,16 @@
                     </div>
                     <div class="sm:col-span-2">
                         <label for="body"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
-                        <input type="text" id="body" name="body"
-                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            class="block mb-2 text-sm font-medium text-gray-900">Body</label>
+                        <input type="text" id="body" name="body "
+                            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
                             placeholder="Write a body here...">
                     </div>
 
                 </div>
                 <div class="flex items-center space-x-4">
                     <button type="submit"
-                        class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                        class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
                         save
                     </button>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,48 +1,20 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
-use Illuminate\Support\Arr;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Post\PostController;
 
 Route::get('/', function () {
-    return view('home', ['title' => 'Home Page']);
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about', ['nama' => 'Gavra Maheswara', 'title' => 'About']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->paginate(9)->withQueryString( )]);
-});
-
-Route::get('/posts/{post:slug}', function (Post $post) {
-    return view('post', ['title' => 'Single Post', 'post' => $post]);
-});
-
-Route::get('/contact', function () {
-    return view('contact', ['title' => 'Contact']);
-});
-
-Route::get('/authors/{user:username}', function (User $user) {
-    // $posts = $user->posts->load('category', 'author');
-
-    return view('posts', ['title' => count($user->posts) . 'Articles by : ' . $user->name, 'posts' => $user->posts]);
-});
-
-Route::get('/categories/{category:slug}', function (Category $category) {
-    // $posts = $category->posts->load('category', 'author');
-
-    return view('posts', ['title' => 'Articles in : ' . $category->name, 'posts' => $category->posts]);
-});
-
-// Route::get('/crud/crud', function() {
-//     return view('/crud/crud', ['title' => 'CRUD Post']);
-// });
-
-Route::get('/crud/crud', [PostController::class, 'index']);
-Route::post('/crud/crud', [PostController::class, 'store']);
-
+require __DIR__.'/auth.php';
